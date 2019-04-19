4110552c7645f4d409223c70bdd66d60e02991be
Salsa <>
2019-04-19 11:59:01 -0700

    got concert api to work

---
+++ liri.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var axios= require("axios");
+//var spotify = new Spotify(keys.spotify);
+
+var userCommand = process.argv[2];
+var userQuery = process.argv[3];
+
+console.log(userCommand);
+console.log(userQuery);
+
+if (userCommand === "concert-this"){
+    console.log("we want to see a concert")
+    getConcertInfo(userQuery)
+
+} else if (userCommand ==="spotify-this-song"){
+    console.log("we want to get this song")
+    getSpotifySong(userQuery)
+
+} else if (userCommand === "movie-this"){
+    console.log("we want to get this movie")
+    getMovieInfo(userQuery)
+
+} else {
+    console.log("none of the prevous conditions were met")
+}
+
+
+function getConcertInfo(artist){
+    console.log("-----------------------------")
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+        function(response) {
+            // If the axios was successful...
+            // Then log the body from the site!
+            console.log(response.data);
+          }
+    )
+    
+};
