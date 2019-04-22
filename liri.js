af5a94aa52ee38737dbb9863254b8e4d778fda7f
Salsa <>
2019-04-21 20:35:07 -0700

    fighting with output on concert, song and movie perameters

--- liri.js
+++ liri.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var axios= require("axios");
+var axios = require("axios");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+var moment = require("moment");
+var fs = require("fs");
 
 var userCommand = process.argv[2];
 var userQuery = process.argv[3];
@@ -10,31 +12,98 @@ var userQuery = process.argv[3];
 console.log(userCommand);
 console.log(userQuery);
 
-if (userCommand === "concert-this"){
+//determine which query is choisen (Example: concert-this, spotify-this-song, movie-this and do-what-it-says)
+if (userCommand === "concert-this") {
     console.log("we want to see a concert")
     getConcertInfo(userQuery)
 
-} else if (userCommand ==="spotify-this-song"){
+} else if (userCommand === "spotify-this-song") {
     console.log("we want to get this song")
     getSpotifySong(userQuery)
 
-} else if (userCommand === "movie-this"){
+} else if (userCommand === "movie-this") {
     console.log("we want to get this movie")
     getMovieInfo(userQuery)
 
+} else if (userCommand === "do-what-it-says") {
+    console.log("do what it says")
+    getMovieInfo(userQuery)
+
 } else {
-    console.log("none of the prevous conditions were met")
+    console.log("none of the previous conditions were met")
 }
 
+// node liri.js for concert-this
 
-function getConcertInfo(artist){
+function getConcertInfo(artist) {
+
     console.log("-----------------------------")
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
-        function(response) {
-            // If the axios was successful...
-            // Then log the body from the site!
-            console.log(response.data);
-          }
-    )
-    
+        function (response) {
+            if (response.data.length <= 0) {
+                console.log("No info for this Artist")
+            } else {
+                for (var i = 0; i < response.data.length; i++) {
+
+                    var currentData = `\n
+            Venue: ${response.data[i].venue.name}
+            Location: ${response.data[i].venue.city + ", " + response.data[0].venue.region}
+            Event Date: ${moment(response.data[i].datetime).format('LL')}
+            `
+                    console.log(currentData)
+                }
+            }
+        });
+}
+
+// node liri.js for spotify-this-song 
+
+
+function getSpotifySong(song) {
+    if (userQuery === "") {
+        userQuery = "ace+of+base+the+sign"
+    }
+    console.log("-----------------------------")
+    spotify.search({
+        type: 'artist,track',
+        query: userQuery
+    },
+        function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            console.log('\n')
+
+            var currentData = `\n
+    Artist: ${data.tracks.items[0].name}
+    Preview:{data.tracks.items[0].artists[0].name}
+    Track: $ ${data.tracks.items[0].preview_url}
+    Album: ${data.tracks.items[0].album.name}
+            `
+            console.log(currentData)
+        });
+
+};
+
+// node liri.js for movie-this
+
+function getMovieInfo(movie) {
+    console.log("-----------------------------")
+    axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=a3f93189").then(
+
+        function (response) {
+
+            var currentData = `\n
+    Title: ${response.data.Title}
+    Released: ${response.data.Year}
+    IMDB Rating: ${response.data.imdbRating}
+    Rotten Tomatos Rating: ${response.data.Ratings[1].Value}
+    Country: ${response.data.Country}
+    Language: ${response.data.Language}
+    Plot: ${response.data.Plot}
+    Actors: ${response.data.Actors}
+            `
+            console.log(currentData)
+
+        })
 };
